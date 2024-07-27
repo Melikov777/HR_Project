1b83e6af8c3011ade7c96662f3db7388e6d3c3f8
Melikov777 <mlkvlkbr320@gmail.com>
2024-07-27 22:13:46 +0400

    added entities

---
+++ HrProject/HrProject.Core/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using HrProject.Core.Interfaces;
+
+namespace HrProject.Core.Entities;
+
+public class Employee : IEntity<Guid>
+{
+    public Guid Id { get; }
+    public string Name { get; set; } = null!;
+    public string? LastName { get; set; }
+    public int DepartmentId { get; set; }
+    public int PositionId { get; set; }
+
+    public Employee(string name , string? lastname, int departmentId, int positionId)
+    {
+        Name = name;
+        LastName = lastname;
+        DepartmentId = departmentId;
+        PositionId = positionId;
+        Id = Guid.NewGuid();
+    }
+}
+
---
+++ HrProject/HrProject.Core/Entities/Position.cs
@@ -0,0 +1,17 @@
+﻿using HrProject.Core.Interfaces;
+
+namespace HrProject.Core.Entities;
+
+public class Position:IEntity<int>
+{
+    public int Id { get; }
+    public string Name { get; set; } = null!;
+
+    private static int _id;
+
+    public Position(string name)
+    {
+        Id = _id++;
+        Name = name;
+    }
+}
