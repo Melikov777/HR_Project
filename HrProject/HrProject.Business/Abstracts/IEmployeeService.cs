ba551337abc096849b51e92d8c8b0183d85f317c
Melikov777 <mlkvlkbr320@gmail.com>
2024-07-29 16:26:37 +0400

    fixed bugs in employee service class

--- HrProject/HrProject.Business/Abstracts/IEmployeeService.cs
+++ HrProject/HrProject.Business/Abstracts/IEmployeeService.cs
@@ -4,9 +4,9 @@ namespace HrProject.Business.Abstracts;
 
 public interface IEmployeeService
 {
-    void Create(string name);
-    void Update(int id, string name);
-    void Delete(int id);
+    void Create(string name, string? lastname, int departmentId, int positionId);
+    void Update(Guid id, string name, string? lastname, int departmentId, int positionId);
+    void Delete(Guid id);
     List<Employee> GetAll();
-    Employee GetById(int id);
+    Employee GetById(Guid id);
 }
